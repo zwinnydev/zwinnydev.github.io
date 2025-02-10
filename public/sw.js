fb37af6fb2376d980686969d5de878bdc4b8b2d4
Maciej Trojniarz <maciej.trojniarz@gmail.com>
2025-02-10 21:59:04 +0100

    remove caches sw.js (#6)

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,16 @@
-console.log("remove orphaned service workers if needed")
-navigator.serviceWorker.getRegistrations().then(registrations => {
+console.log("remove old gatsby cache and service workers")
+
+if(caches)
+{caches.keys().then(function(names) {
+    for (let name of names)
+        caches.delete(name);
+});}
+
+if(navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
     for (const registration of registrations) {
         console.log(registration)
         registration.unregister();
     } 
 });
+}
