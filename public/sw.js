ba48442967acce3c1c9d348fefaaf53ad427206b
Maciej Trojniarz <maciej.trojniarz@gmail.com>
2025-02-10 21:55:50 +0100

    force gatsby worker removal with caches

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,11 @@
-console.log("remove orphaned service workers if needed")
-navigator.serviceWorker.getRegistrations().then(registrations => {
+console.log("remove old site gatsby caches and sw")
+
+caches?.keys().then(function(names) {
+    for (let name of names)
+        caches.delete(name);
+});
+
+navigator?.serviceWorker?.getRegistrations()?.then(registrations => {
     for (const registration of registrations) {
         console.log(registration)
         registration.unregister();
